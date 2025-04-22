53a7506a6c8b73fe7b62dd2ee441064ed2c7cdc5
youji44 <ansonmaster810@gmail.com>
2025-04-22 17:23:31 +0800

    updated form validate

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
